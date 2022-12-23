51194d4905bc7ac12540ee3dd1bf69f48d2325a0
ypolosov <ypolosov@gmail.com>
2022-12-23 08:57:08 +0000

    refactor post-create.sh

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+LOCK_FILE="package-lock.json" 
+
 pushd frontend
 npm ci
+if [ -f $LOCK_FILE ]; then
+   npm ci
+else
+   npm i
+fi
 popd
 
 pushd backend
 npm ci
+if [ -f $LOCK_FILE ]; then
+   npm ci
+else
+   npm i
+fi
 popd
