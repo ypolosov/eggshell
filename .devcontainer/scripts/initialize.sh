2fd40929be65f5232e23edcfaed820fe553ed577
ypolosov <ypolosov@gmail.com>
2022-12-26 12:44:19 +0000

    use the bind mounts

--- .devcontainer/scripts/initialize.sh
+++ .devcontainer/scripts/initialize.sh
@@ -5,15 +5,15 @@
 find ~/.ssh/ -type f -exec grep -l 'PRIVATE' {} \; | xargs ssh-add
 
 #######
-if docker volume create --name eggshell-volume &> /dev/null; then
-  echo "Created volume eggshell-volume"
-else
-  echo "Failed to create volume eggshell-volume"
-fi
+# if docker volume create --name eggshell-volume &> /dev/null; then
+#   echo "Created volume eggshell-volume"
+# else
+#   echo "Failed to create volume eggshell-volume"
+# fi
 
-docker network create eggshell-network &> /dev/null
-if [ "$?" -ne "0" ]; then
-  echo "Network eggshell-network already exists"
-else
-  echo "Created docker network eggshell-network"
-fi
+# docker network create eggshell-network &> /dev/null
+# if [ "$?" -ne "0" ]; then
+#   echo "Network eggshell-network already exists"
+# else
+#   echo "Created docker network eggshell-network"
+# fi
