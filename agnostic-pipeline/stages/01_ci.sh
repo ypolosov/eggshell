4d822ee0c3e30f07f48af5ceb659e93e3cb222f8
ypolosov <ypolosov@gmail.com>
2022-12-30 12:48:40 +0000

    check

--- agnostic-pipeline/stages/01_ci.sh
+++ agnostic-pipeline/stages/01_ci.sh
@@ -6,8 +6,22 @@ echo "Ci Application"
 
 set -x
 
+CONFIG_PATH="$PROJECT_DIR/.env"
+cat "${CONFIG_PATH}"
+source "${CONFIG_PATH}"
+
 echo "Install devcontainers/cli..."
 npm install -g @devcontainers/cli
+
+echo "Container registry login..."
+if [[ ! -z "${DOCKER_PASSWORD}" ]]
+then
+    # cloud running
+    echo "${DOCKER_PASSWORD?:}" | docker login -u "${ARCHIVE_ACCOUNT?:}" --password-stdin "${ARCHIVE_REGISTRY}"
+else
+    # local running
+    docker login
+fi
 
 echo "Build and push devcontainer..."
 devcontainer build --workspace-folder . --image-name docker.io/ypolosov/eggshell:1.1.1
