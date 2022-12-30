b20bacd9ed0a00bca315e59e01b602ba0cffaabb
ypolosov <ypolosov@gmail.com>
2022-12-30 12:09:35 +0000

    check

--- agnostic-pipeline/stages/01_ci.sh
+++ agnostic-pipeline/stages/01_ci.sh
@@ -6,6 +6,9 @@ echo "Ci Application"
 
 set -x
 
+echo "Install devcontainers/cli..."
+npm install -g @devcontainers/cli
+
 echo "Build and push devcontainer..."
 devcontainer build --workspace-folder . --push true --image-name docker.io/ypolosov/eggshell:1.1.1
 
