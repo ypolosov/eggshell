36f9e8cc8897391644aabeb5bd2cb71b26b26fe3
ypolosov <ypolosov@gmail.com>
2022-12-30 11:56:09 +0000

    setup @devcontainers/cli

--- .devcontainer/scripts/initialize.sh
+++ .devcontainer/scripts/initialize.sh
@@ -4,16 +4,16 @@
 # open issue https://github.com/microsoft/vscode-remote-release/issues/4024?ysclid=lbz6geepsn205383988
 find ~/.ssh/ -type f -exec grep -l 'PRIVATE' {} \; | xargs ssh-add
 
-#######
-if docker volume create --name eggshell-volume &> /dev/null; then
-  echo "Created volume eggshell-volume"
-else
-  echo "Failed to create volume eggshell-volume"
-fi
+# #######
+# if docker volume create --name eggshell-volume &> /dev/null; then
+#   echo "Created volume eggshell-volume"
+# else
+#   echo "Failed to create volume eggshell-volume"
+# fi
 
-docker network create eggshell-network &> /dev/null
-if [ "$?" -ne "0" ]; then
-  echo "Network eggshell-network already exists"
-else
-  echo "Created docker network eggshell-network"
-fi
+# docker network create eggshell-network &> /dev/null
+# if [ "$?" -ne "0" ]; then
+#   echo "Network eggshell-network already exists"
+# else
+#   echo "Created docker network eggshell-network"
+# fi
