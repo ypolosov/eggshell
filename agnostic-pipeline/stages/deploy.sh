4f6732a967f04ad4111e6a5110066d0de49c006c
ypolosov <ypolosov@gmail.com>
2023-01-24 07:59:59 +0000

    sdfsd

--- agnostic-pipeline/stages/deploy.sh
+++ agnostic-pipeline/stages/deploy.sh
@@ -7,6 +7,14 @@ ls -la
 cd ./infra/cluster/aws
 echo TFE_TOKEN=$TFE_TOKEN
 export TF_LOG=debug
+# Create .terraformrc with credential config for user
+cat >~/.terraformrc <<EOL
+credentials "app.terraform.io" {
+  token = "${TFE_TOKEN}"
+}
+EOL
+cat ~/.terraformrc
+
 terraform init
 
 terraform apply -auto-approve
