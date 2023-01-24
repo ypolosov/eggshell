eca5c716beac559c0608a56780a5ab155372c8b6
ypolosov <ypolosov@gmail.com>
2023-01-24 08:54:46 +0000

    add aws instance

--- infra/cluster/aws/main.tf
+++ infra/cluster/aws/main.tf
@@ -17,3 +17,13 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+resource "aws_instance" "default" {
+  ami                         = "ami-0afd55c0c8a52973a"    # ОС (Ubuntu, 22.04 LTS)
+  instance_type               = "t3.micro"                 # тип процессора и ресурс машины (CPU и RAM)
+  subnet_id                   = "subnet-083e6806ee6be3c7e" # одна из дефолтных подсетей
+  associate_public_ip_address = true                       # автоматически установить динамический ip
+  tags = {
+    Name = "test-instance"
+  }
+}
