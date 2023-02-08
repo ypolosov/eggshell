abceaa4cc097123973639a003349d28737775023
ypolosov <ypolosov@gmail.com>
2023-02-08 15:17:23 +0000

    asdfas

--- terraform/learn-terraform-yc-instance/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "ssh_public_key" {
-  description = "SSH public key"
-  type        = string
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHT05Kbx6j2qktJ8717JVBND7o/1+dVIYr2Hkw+pEteu ypolosov@gmail.com"
-}
