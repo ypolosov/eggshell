95171b9dd64c12ee6b70395f5f72db01a8dad8e5
ypolosov <ypolosov@gmail.com>
2023-02-10 13:22:15 +0000

    increase computing

--- infra/cluster/aws/main.tf
+++ infra/cluster/aws/main.tf
@@ -78,6 +78,16 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
       max_size     = 2
       desired_size = 1
     }
