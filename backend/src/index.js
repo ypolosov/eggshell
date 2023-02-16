f61d87b9c82f91618e374d6f746aa02e0942324c
ypolosov <ypolosov@gmail.com>
2023-02-16 15:03:19 +0000

    dfgdf

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 5000;
 const appName = 'Backend';
-const version = '0.1.1';
+const version = '0.1.0';
 
 const server = http.createServer((req, res) => {
 
